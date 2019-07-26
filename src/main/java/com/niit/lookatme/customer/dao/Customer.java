d19f0ddb0557c7de01c8eaccd1fe65617a3931aa
Udit Goel <udit_goel1@optum.com>
2019-07-26 20:22:43 +0530

    pom updates

--- src/main/java/com/niit/lookatme/customer/dao/Customer.java
+++ src/main/java/com/niit/lookatme/customer/dao/Customer.java
@@ -18,8 +18,8 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hibernate.annotations.ColumnDefault;
 
 import com.niit.lookatme.dao.Address;
--- src/main/java/com/niit/lookatme/dao/Employee.java
+++ src/main/java/com/niit/lookatme/dao/Employee.java
@@ -20,8 +20,8 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.hibernate.annotations.ColumnDefault;
 
 /**
